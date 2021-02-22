7bbab66960e32aa8aefdda63e902f192763c8492
Guru <gurumurthy.ramamurthy@gmail.com>
2021-02-22 12:01:24 +0530

    Created a customer object with list of stocks

--- MyApp5/src/main/java/com/ofss/TestProduct.java
+++ MyApp5/src/main/java/com/ofss/TestProduct.java
@@ -5,7 +5,13 @@ public class TestProduct {
 	public static void main(String[] args) {
 		System.out.println("Starting the application");
 		Stock st1=new Stock(100, "ORCL", 123.4);
-		System.out.println(st1);
+		Stock st2=new Stock(101, "IBM", 223.4);
+		Stock st3=new Stock(102, "WIPRO", 323.4);
+		Stock stocks[]= {st1,st2,st3}; // including all stock objects into stock array
+		
+		Customer c1=new Customer("Guru", stocks);
+		
+		System.out.println(c1);
 		System.out.println("=========================");
 				
 
